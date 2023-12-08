4acec84768efc141aeff617f12ecdc9af67af970
Lavish Palia <lavishgarg1199@gmail.com>
2023-12-08 22:23:23 +0530

    mongodb atlas db setup and morgan logging middleware

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// import routes
-const bootcampRoutes = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: 'config/config.env' });
 
+// connect to database
+connectDB();
+
+// import routes
+const bootcampRoutes = require('./routes/bootcamps');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Dev logging middleware
+app.use(morgan('dev'));
 
 // mount the routes
 app.use('/api/v1/bootcamps', bootcampRoutes);
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // close the server & exit process
+  server.close(() => process.exit(1));
+});
